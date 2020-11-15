1d3b6154660cecc52f10220b41eda1d873961071
danielzairin <danielzairin@student.usm.my>
2020-11-15 22:02:01 +0800

    Added test data

---
+++ src/testData.js
@@ -0,0 +1,45 @@
+export let pantryItems = [
+  {
+    id: 1,
+    name: "Milk",
+    expiryDate: new Date(2020, 10, 15),
+  },
+  {
+    id: 2,
+    name: "Banana",
+    expiryDate: new Date(2020, 10, 20),
+  },
+  {
+    id: 3,
+    name: "Chicken",
+    expiryDate: new Date(2020, 10, 30),
+  },
+  {
+    id: 4,
+    name: "Rice",
+    expiryDate: new Date(2020, 11, 15),
+  },
+];
+
+export let groceryListItems = [
+  {
+    id: 1,
+    name: "Flour",
+    checked: false,
+  },
+  {
+    id: 2,
+    name: "Cheese",
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Milk",
+    checked: false,
+  },
+  {
+    id: 4,
+    name: "Tomatoes",
+    checked: false,
+  },
+];
