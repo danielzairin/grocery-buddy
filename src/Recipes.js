2ed6c762e9be9cce60cab095db6057cc21068d13
danielzairin <danielzairin@student.usm.my>
2020-12-20 14:27:58 +0800

    Updated Recipes components
    Co-authored-by: atikah <jkim46175@gmail.com>

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { pantryItems } from "./testData";
 
 function Recipes() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    // Get list of ingredients
+    const ingredients = pantryItems.map((item) => item.name).toString();
+
+    // Fetch recipes and set it to recipes state
+    fetch(
+      "https://api.spoonacular.com/recipes/findByIngredients?apiKey=API_KEY&ingredients=" +
+        ingredients
+    )
+      .then((response) => response.json())
+      .then((json) => setRecipes(json));
+  }, []);
+
   return (
     <div>
       <h1>Recipes</h1>
+      {/* {recipes.map((recipe) => (
+        <div>
+          <h2>{recipe.title}</h2>
+
+          <img src={recipe.image} />
+
+          <p>Used ingredients:</p>
+          <ol>
+            {recipe.usedIngredients.map((ingredient) => (
+              <li>{ingredient.name}</li>
+            ))}
+          </ol>
+
+          <p>Missing ingredients:</p>
+          <ol>
+            {recipe.missedIngredients.map((ingredient) => (
+              <li>{ingredient.name}</li>
+            ))}
+          </ol>
+        </div>
+      ))} */}
     </div>
   );
 }
