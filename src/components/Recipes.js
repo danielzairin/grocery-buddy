a416796f396f2ae18718a65b937a0ff937841b52
danielzairin <danielzairin@student.usm.my>
2020-12-28 20:23:14 +0800

    Added test data for recipes

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,46 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { pantryItems } from "../testData";
+import { recipes as fetchedRecipes } from "../testData";
 
 function Recipes() {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     // Get list of ingredients
-    const ingredients = pantryItems.map((item) => item.name).toString();
+    // const ingredients = pantryItems.map((item) => item.name).toString();
 
     // Fetch recipes and set it to recipes state
-    fetch(
-      "https://api.spoonacular.com/recipes/findByIngredients?apiKey=API_KEY&ingredients=" +
-        ingredients
-    )
-      .then((response) => response.json())
-      .then((json) => setRecipes(json));
+    // fetch(
+    //   "https://api.spoonacular.com/recipes/findByIngredients?apiKey=API_KEY&ingredients=" +
+    //     ingredients
+    // )
+    //   .then((response) => response.json())
+    //   .then((json) => setRecipes(json));
+
+    // Sample API response
+    setRecipes(fetchedRecipes);
   }, []);
 
   return (
     <div>
       <h1>Recipes</h1>
-      {/* {recipes.map((recipe) => (
-        <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
           <h2>{recipe.title}</h2>
 
           <img src={recipe.image} />
 
           <p>Used ingredients:</p>
-          <ol>
+          <ul>
             {recipe.usedIngredients.map((ingredient) => (
-              <li>{ingredient.name}</li>
+              <li key={ingredient.id}>{ingredient.name}</li>
             ))}
-          </ol>
+          </ul>
 
           <p>Missing ingredients:</p>
-          <ol>
+          <ul>
             {recipe.missedIngredients.map((ingredient) => (
-              <li>{ingredient.name}</li>
+              <li key={ingredient.id}>{ingredient.name}</li>
             ))}
-          </ol>
+          </ul>
         </div>
-      ))} */}
+      ))}
     </div>
   );
 }
