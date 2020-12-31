8af72a9ba3c32daa7dc43d82c6bcbb20fa7ae892
yanaismail <iliyanaismail99@gmail.com>
2020-12-31 14:41:08 +0800

    Update design in pantry

--- src/components/Pantry.js
+++ src/components/Pantry.js
@@ -30,33 +30,66 @@ const sortedpantryItems = pantryItems.sort((a,b) => a.expiryDate - b.expiryDate)
  
   return (
     <div>
-        <h1>Pantry</h1>
-        <h5>Today : {dt} </h5> 
+        <h1
+        className="text-center fixed-top w-100 bg-primary shadow-sm"
+        style={{ height: "55px" }}>
+              Pantry
+      </h1>
+        
 
-      <form onSubmit={addPantry}>
-        <span>Add item: </span>
-        <input type="text" placeholder="Item name" name="name" />
-        <span>Add Expiry Date: </span>
-        <input type="Date" placeholder="Expiry Date" name="expiryDate" />
-        <button>Add</button>
-      </form>
+      <div class="card text-center">
+        <div className="card shadow">
+          <div className="card-body bg-warning">
+            <h5>TODAY : {dt} </h5> 
 
-      <ul> <h3>Items in Pantry:</h3>
-            {items.map((item) => (
+        <form onSubmit={addPantry}>
+         <span>ADD ITEM: </span>
+          <input 
+          className="form-control rounded-0"
+          type="text" 
+          placeholder="Item name" 
+          name="name"  
+          />
+         <span> ADD EXPIRY DATE: </span>
+          <input 
+          className="form-control rounded-0"
+          type="Date" 
+          placeholder="Expiry Date" 
+          name="expiryDate" />
+           <button className="btn btn-primary btn-block rounded-0">
+             Add
+           </button>
+          </form>
+     
+         </div>
+        </div>
+      </div>
+
+      <ul> 
+      
+        <div className="card text-center">    
+          <div className="card shadow">
+            <div className="card-body bg-primary">
+            <h3>ITEMS IN PANTRY:</h3>
+              </div>   
+            </div>
+          </div>
+         
+
+        {items.map((item) => (
          <li key={item.id}>
-           <span>{item.name}  {item.expiryDate.toLocaleDateString()  }</span>
-
-           <span><h7>  Item expired in {Math.floor((item.expiryDate-showdate)/(1000*60*60*24)) } days.</h7></span>
-          </li>
+           <div className="card text-dark bg-light mb-3" ></div>
+           <span>
+             <div class="card-body">    
+              <h5 className="card-tittle">{item.name}</h5>
+           {item.expiryDate.toLocaleDateString() } </div></span> 
+           <span><h7>Item expired in {Math.floor((item.expiryDate-showdate)/(1000*60*60*24)) } days.</h7></span>
+         </li>
         
          ))}
       </ul>
 
-     
-         
-      
-     
-    </div>
+        </div>
   );
 }
 
